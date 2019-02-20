23480fd3feda9f204f9ba7f89aa86a630ef69676
tesla <1031721128@qq.com>
2019-02-20 10:10:33 +0800

    22

--- sql.sql
+++ sql.sql
@@ -6,3 +6,12 @@ create table students
 		age tinyint unsigned not null,
 		tel char(13) null default "-"
 	);
+
+create table students
+	(
+		id int unsigned not null auto_increment primary key,
+		name char(8) not null,
+		sex char(4) not null,
+		age tinyint unsigned not null,
+		tel char(13) null default "-"
+	);
